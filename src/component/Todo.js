cce0af7151e4c5b6c744e23c801fc32d21b01193
ShirazMuhd <shirazmuhdofficial@gmail.com>
2021-05-19 12:07:23 +0530

    toDo prototype completed

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -12,22 +12,24 @@ function Todo() {
         <h1>ToDo List</h1>
       </div>
       <div className="input">
-        <input value={toDo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setTodos([...toDos, Todo])} className="fas fa-plus"></i>
+        <input  onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setTodos([...toDos, toDo])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {
-
-          <div className="todo">
-            <div className="left">
-              <input type="checkbox" name="" id="" />
-              <p>Rect tutorial</p>
-            </div>
-            <div className="right">
-              <i className="fas fa-times"></i>
-            </div>
-          </div>
-
+          toDos.map((value) => {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input type="checkbox" name="" id="" />
+                  <p>{value}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
+              </div>
+            )
+          })
         }
       </div>
     </div>
